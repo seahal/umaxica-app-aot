f8dee530e4b87aafe3d7221aec9ba828110e9fba
seahal <seahal.com@gmail.com>
2025-04-03 22:08:47 +0900

    [system] biome added.

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { Hono } from 'hono/quick'
+import { cors } from 'hono/cors'
+import { compress } from 'hono/compress'
+import { csrf } from 'hono/csrf'
+import { timeout } from 'hono/timeout'
+import { logger } from 'hono/logger'
+import { prettyJSON } from 'hono/pretty-json'
+import {
+  trimTrailingSlash,
+} from 'hono/trailing-slash'
+import { bodyLimit } from 'hono/body-limit'
+import { secureHeaders } from 'hono/secure-headers'
+
+const app = new Hono()
+
+
+app.use(cors())
+app.use(compress())
+app.use(csrf())
+app.use(timeout(2000))
+app.use(logger())
+app.use(prettyJSON())
+app.use(secureHeaders())
+app.use(trimTrailingSlash())
+
+
+app.get('/', (c) => {
+  return c.html('Hello fastly!')
+})
+app.get('/uma', (c) => c.text('xica!'))
+app.get('/two', (c) => {
+  return c.json({ message: 'car!' })
+})
+app.get('/about/me', (c) => c.text('Without Trailing Slash'))
+
+
+app.fire()
